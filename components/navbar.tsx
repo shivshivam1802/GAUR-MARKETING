f3a71a081b6624d0493263af1732d6db8e39898b
Shivam Kumar <shivshivam747@gmail.com>
2026-01-15 15:50:40 +0530

    Update navbar title from 'INTERNET SERVICE PROVIDER' to 'Suhana Beverages'

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -27,7 +27,7 @@ export function Navbar() {
         <div className="flex items-center justify-between h-20">
           <Link href="/" className="flex items-center space-x-2">
             <div className="text-2xl font-bold font-[family-name:var(--font-poppins)] text-white">
-              INTERNET SERVICE  <span className="text-accent">PROVIDER </span>
+              Suhana <span className="text-accent">Beverages </span>
             </div>
           </Link>
 
