44f676cf17b48539a985ade9e42e25fb772b5dd0
Shivam Kumar <shivshivam747@gmail.com>
2026-04-24 16:50:01 +0530

    Refactor page component to use redirect from Next.js

--- app/link/page.tsx
+++ app/link/page.tsx
@@ -1,16 +1,9 @@
-"use client";
+import { redirect } from "next/navigation";
 
-import { useEffect } from "react";
-
-export default function Page() {
-  useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    const url = params.get("url");
-
-    if (url) {
-      window.location.href = url;
-    }
-  }, []);
-
-  return <div>Redirecting...</div>;
+export default function Page({
+  searchParams,
+}: {
+  searchParams: { url?: string };
+}) {
+  redirect(searchParams.url || "/");
 }
